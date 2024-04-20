f129fdcc9b4b838f0227120c2f3b4e9e08704000
Rajithxx <148357145+Rajithxx@users.noreply.github.com>
2024-04-20 12:54:36 +0530

    212221060222 HALF_SUBTRACTOR.v
    Rajith Rufus

--- HALF_SUBTRACTOR.srcs/sources_1/new/HALF_SUBTRACTOR.v
+++ HALF_SUBTRACTOR.srcs/sources_1/new/HALF_SUBTRACTOR.v
@@ -1,28 +1,8 @@
-`timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 05.01.2024 14:42:43
-// Design Name: 
-// Module Name: HALF_SUBTRACTOR
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
-
-module half_subtractor( D,Bo,A,B);
-input A,B;
-output D,Bo;
-                      //Write the logic for half subtractor in Dataflow modeling
-
-endmodule  
+module hs(a,b,diff,borrow);
+input a,b;
+output diff,borrow;
+wire w1;
+xor g1(diff,a,b);
+not g2(w1,a);
+and g3(borrow,w1,b);
+endmodule
